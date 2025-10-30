758e1260919650a4ba12737cd4509ec339887a8c
Ajopratama1 <a.ajopratama01@gmail.com>
2025-10-30 12:57:36 +0700

    Add OutlinedTextField for complete address (Alamat Lengkap)

--- app/src/main/java/com/example/questuserinput_057/TeksEdit.kt
+++ app/src/main/java/com/example/questuserinput_057/TeksEdit.kt
@@ -1,56 +1,83 @@
-package com.example.questuserinput_057
+package com.example.QuestUserInput_057
 
-import androidx.compose.runtime.Composable
-import android.R
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.material3.Button
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.example.questuserinput_057.R
 
-@composable
-fun FormDataDiri(modifier: Modifier
-){
-    var textNama by remember {mutableStateOf(value="")}
-    var textAlamat by remember {mutableStateOf(value="")}
-    var textJK by remember {mutableStateOf(value="")}
+@Composable
+fun FormDataDiri(modifier: Modifier) {
+    var textNama by remember { mutableStateOf( "" )  }
+    var textAlamat by remember { mutableStateOf("") }
+    var textJK by remember { mutableStateOf("") }
 
-    var nama by remember {mutableStateOf(value="")}
-    var alamat by remember {mutableStateOf(value="")}
-    var jenis by remember {mutableStateOf(value="")}
+    var nama by remember { mutableStateOf("") }
+    var alamat by remember { mutableStateOf("") }
+    var jenis by remember { mutableStateOf("") }
 
-    val gender:List<R.string> = listOf("Laki-laki","Perempuan")
+    val gender:List<String> = listOf("Laki-laki", "Perempuan")
 
-    comlumn(modifier = modifier.padding(top = 50.dp),
+    Column(modifier = Modifier.padding(top = 50.dp),
         verticalArrangement = Arrangement.Top,
-        horizontalAlignment = Alignment.CenterHorizontally {
-            OutlinedTextField(
-                value = textNama,
-                singleLine = true,
-                shape = MaterialTheme.shapes.large,
-                modifier = Modifier.width(250.dp),
-                label = {Text(text = "Nama Lengkap")},
-                onValueChange = {textNama = it}
-            )
-            Row {
-                gender.forEach { item ->
-                    Row(modifier = Modifier.selectable(
+        horizontalAlignment = Alignment.CenterHorizontally) {
+        OutlinedTextField(
+            value = textNama,
+            singleLine = true,
+            shape = MaterialTheme.shapes.large,
+            modifier = Modifier.width(250.dp),
+            label = { Text(text = " Nama Lengkap") },
+            onValueChange = {
+                textNama = it
+            }
+        )
+        Row {
+            gender.forEach { item ->
+                Row(
+                    modifier = Modifier.selectable(
                         selected = textJK == item,
-                        onClick = {textJK = item}
+                        onClick = { textJK = item }
                     ), verticalAlignment = Alignment.CenterVertically) {
-                        RadioButton(
-                            selected = textJK == item,
-                            onClick = {textJK = item}
-                        )
-                        Text(text = item)
-                    }
-
+                    RadioButton(
+                        selected = textJK == item,
+                        onClick = {
+                            textJK = item
+                        })
+                    Text(item)
                 }
             }
-
-
         }
-
-
-
+        OutlinedTextField(
+            value = textAlamat,
+            singleLine = true,
+            modifier = Modifier.width(250.dp),
+            label = {Text(text = "Alamat Lengkap")},
+            onValueChange = {
+                textAlamat = it
+            }
+        )
 
